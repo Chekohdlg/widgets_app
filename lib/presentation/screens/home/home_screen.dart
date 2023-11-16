5217f89d4c9727824f060c7bce97bcb4ae2cf4ce
sergio hidalgo <sergio.hidalgorobles@gmail.com>
2023-11-15 22:39:36 -0600

    go router configured

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu-items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screens.dart';
 
@@ -62,7 +63,8 @@ class _CustomListTile extends StatelessWidget {
         // Navigator.of(context).push(MaterialPageRoute(
         //   builder: (context) => const ButtonsScreen(),
         // ));
-        Navigator.pushNamed(context, menuItem.link);
+        // Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
