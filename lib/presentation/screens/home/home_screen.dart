4795d9997499d4e187be528c3e2e7e4fbfb5e551
sergio hidalgo <sergio.hidalgorobles@gmail.com>
2023-11-15 22:48:48 -0600

    go route named

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu-items.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screens.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -60,11 +61,9 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        // Navigator.of(context).push(MaterialPageRoute(
-        //   builder: (context) => const ButtonsScreen(),
-        // ));
-        // Navigator.pushNamed(context, menuItem.link);
-        context.push(menuItem.link);
+        // context.push(menuItem.link); //go router
+
+        context.pushNamed(CardsScreen.name); // go router named
       },
     );
   }
