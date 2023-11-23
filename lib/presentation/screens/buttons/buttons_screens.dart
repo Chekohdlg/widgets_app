abcf1f26071f4e831971c8daed536927bd3bec0a
sergio hidalgo <sergio.hidalgorobles@gmail.com>
2023-11-22 23:15:18 -0600

    add custom button

--- lib/presentation/screens/buttons/buttons_screens.dart
+++ lib/presentation/screens/buttons/buttons_screens.dart
@@ -59,6 +59,7 @@ class _ButtonsView extends StatelessWidget {
                 onPressed: () {},
                 icon: const Icon(Icons.account_box_outlined),
                 label: const Text("Text Icon")),
+            const CustomButton(),
             IconButton(
               onPressed: () {},
               icon: const Icon(Icons.app_registration_rounded),
@@ -71,6 +72,31 @@ class _ButtonsView extends StatelessWidget {
                   iconColor: const MaterialStatePropertyAll(Colors.white)),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola adfasdfasdf ',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ),
       ),
     );
